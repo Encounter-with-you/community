a12d05fc9d309265ba3abeca5cfcd15b5dfa19b4
Encounter-with-you <2461864879@qq.com>
2020-01-07 16:34:51 +0800

    add check cookies

--- src/main/java/life/majing/community/conmmunity/controller/IndexController.java
+++ src/main/java/life/majing/community/conmmunity/controller/IndexController.java
@@ -1,16 +1,34 @@
 package life.majing.community.conmmunity.controller;
 
+import life.majing.community.conmmunity.mapper.UserMappper;
+import life.majing.community.conmmunity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMappper userMappper;
+
     @GetMapping("/")
-    public String index(Model model){
-        //model.addAttribute("name",name);
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMappper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
--- src/main/java/life/majing/community/conmmunity/mapper/UserMappper.java
+++ src/main/java/life/majing/community/conmmunity/mapper/UserMappper.java
@@ -3,10 +3,15 @@ package life.majing.community.conmmunity.mapper;
 import life.majing.community.conmmunity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface  UserMappper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) " +
             "values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("Select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
